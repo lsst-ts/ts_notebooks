d38f5ab3b86e1f082dd9e9727274e798a03723b8
Patrick Ingraham <pingraham@lsst.org>
2019-12-06 23:09:50 +0000

    added lab notebooks that have useful info

---
+++ pingraham/lab_notebooks/utils/fitExposure.py
@@ -0,0 +1,63 @@
+import numpy as np
+import matplotlib.pyplot as plt
+    
+def fit2DGaussian(exposure, plot=True):
+    """Gaussian fitting algorithm for LSST Stack exposure objects
+    Uses astroPy model fitting of 2D gaussian, but on a stack image.
+    Parameters
+    ----------
+    exposure : ``ExposureF in module lsst.afw.image.exposure.exposure object``
+        Exposure object from LSST stack
+    plot : `Boolean` (default = True)
+        Plots the data, fit and residuals.
+    Raises
+    ------
+    Nothing for the moment
+    
+    Returns
+    -------
+    fit object, y, x : ``Gaussian2D in module astropy.modeling.functional_models object`` , ndarray object, ndarray object
+            Fit object with all parameters, 2d pixel maps in x and y for plotting
+    Notes
+    -----
+    This is an algorithm to quickly fit 2D-gaussians to LSST stack objects.
+    This is meant to be used with sub-images/stamps where the star is already
+    at the center of the stamp.
+    """
+  
+    import warnings
+    from astropy.modeling import models, fitting
+    
+    # Generate x,y,z data, where z is the 2d data
+    z=exposure.image.array
+    bbox=exposure.getBBox()
+    y, x = np.mgrid[bbox.getBeginY():bbox.getEndY(), bbox.getBeginX():bbox.getEndX()]
+
+    # Fit the data using astropy.modeling
+    #p_init = models.Gaussian2D(amplitude=np.nanmax(z),x_mean=bbox.getCenterX(), y_mean=bbox.getCenterY(), fixed={'theta':True})
+    p_init = models.Gaussian2D(amplitude=np.nanmax(z),x_mean=(bbox.getBeginX()+bbox.getEndX())/2.0, 
+                               y_mean=(bbox.getBeginY()+bbox.getEndY())/2.0, 
+                               x_stddev=1.0,y_stddev=1.0,fixed={'theta':True})
+    fit_p = fitting.LevMarLSQFitter()
+
+    with warnings.catch_warnings():
+        # Ignore model linearity warning from the fitter
+        warnings.simplefilter('ignore')
+        p = fit_p(p_init, x, y, z)
+
+    # Plot the data with the best-fit model
+    if plot == True:
+        plt.figure(figsize=(13, 5))
+        plt.subplot(1, 3, 1)
+        extent=[bbox.getBeginX(),bbox.getEndX(), bbox.getBeginY(),bbox.getEndY()]
+        plt.imshow(z, origin='lower', interpolation='nearest', extent=extent)#, vmin=-1e4, vmax=5e4)
+        plt.title("Data")
+        plt.subplot(1, 3, 2)
+        plt.imshow(p(x, y), origin='lower', interpolation='nearest', extent=extent)#, vmin=-1e4, vmax=5e4)
+        plt.title("Model")
+        plt.subplot(1, 3, 3)
+        plt.imshow(z - p(x, y), origin='lower', interpolation='nearest', extent=extent)#, vmin=-1e4, vmax=5e4)
+        plt.title("Residual")
+        print(repr(p))
+            
+    return(p, y, x)
---
+++ pingraham/lab_notebooks/utils/processExposure.py
@@ -0,0 +1,48 @@
+def processExposure(exposure, bias=None, defects=None, repair=False, repo=None) :
+    from lsst.ip.isr import IsrTask
+    import lsst.daf.persistence as dafPersist
+    import time
+
+    config = IsrTask.ConfigClass()
+    
+    if (bias==None or defects ==None) and repo == None:
+        raise(AttributeError('Repo keyword not set and is required to find bias/defect frames'))
+    else:
+        butler = dafPersist.Butler(repo)
+    
+    if False:
+        config.overscanFitType = "AKIMA_SPLINE"
+        config.overscanOrder = 5
+    else:
+        config.overscanFitType = "POLY"
+        config.overscanOrder = 3
+
+    config.doBias = True
+    if config.doBias == True and bias == None:
+        bias = butler.get('bias', visit=exposure.getInfo().getVisitInfo().getExposureId(), )
+        
+    config.doDark = False
+    config.doFringe = False
+    config.doFlat = False
+    config.doLinearize = False
+    # TODO: should be able to run this but there are issues with defects in the stack at the moment
+    config.doDefect = True if repair == True else False   
+    if (config.doDefect == True and bias == None):
+
+        butler = dafPersist.Butler(repo)
+        defects = butler.get('defects', visit=exposure.getInfo().getVisitInfo().getExposureId(), )
+        
+    config.doAddDistortionModel = False
+    config.doSaturationInterpolation = False
+    config.overscanNumLeadingColumnsToSkip = 20
+
+    isrTask = IsrTask(config=config)
+    
+    # Run ISR
+    start = time.time()
+    isr_corrected_exposure = isrTask.run(exposure, bias=bias, defects=defects).exposure
+    end = time.time()
+    print('Time to perform image ISR was {0:2f} [s]'.format(end - start))
+    
+    return(isr_corrected_exposure)
+
