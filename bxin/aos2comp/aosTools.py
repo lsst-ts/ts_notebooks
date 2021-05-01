a5c1a31213f9bef4dd0421dba0554e867cd98595
Bo Xin <bxin@lsst.org>
2021-05-01 22:33:01 +0000

    add ptg2comp.ipynb

--- bxin/aos2comp/aosTools.py
+++ bxin/aos2comp/aosTools.py
@@ -1,13 +1,16 @@
 import os
 import asyncio
 import numpy as np
+import time
 
 import pandas as pd
 from matplotlib import pyplot as plt
 
 from M1M3_FATABLE import *
 
+from lsst.ts import salobj
 from lsst.ts.idl.enums import MTM1M3
+from lsst.ts.idl.enums import MTHexapod
 
 m3ORC = 2.508
 m3IRC = 0.550
@@ -25,6 +28,59 @@ aa = np.loadtxt('%s/notebooks/M2_FEA/data/M2_1um_72_force.txt'%(os.environ["HOME
 m2_xact = -aa[:,2]
 m2_yact = -aa[:,1]
 
+class MyLogHandler:
+    def __init__(self, nmsg):
+        self.nmsg = nmsg
+        self.nprint = 0
+    def printLogMessage(self, data):
+        if self.nprint < self.nmsg:
+            print(f"{data.level}: {data.message}")
+        self.nprint += 1
+
+async def checkAOSCompStates(m1m3, m2, camhex, m2hex):
+
+    #m1m3
+    sstate = await m1m3.evt_summaryState.aget(timeout=5)
+    print('starting with: M1M3 state',salobj.State(sstate.summaryState), pd.to_datetime(sstate.private_sndStamp, unit='s'))
+    dstate = await m1m3.evt_detailedState.aget(timeout=200)
+    print('          detailed state', MTM1M3.DetailedState(dstate.detailedState), 
+          pd.to_datetime(dstate.private_sndStamp, unit='s'))
+
+    #m2
+    sstate = await m2.evt_summaryState.aget(timeout=5)
+    print('staring with: m2 state',salobj.State(sstate.summaryState), pd.to_datetime(sstate.private_sndStamp, unit='s'))
+
+    #camhex
+    state = await camhex.evt_summaryState.aget(timeout=5)
+    print('staring with: cam hex state', salobj.State(state.summaryState), pd.to_datetime(state.private_sndStamp, unit='s'))
+    dstate = await camhex.evt_controllerState.aget(timeout=5)
+    print('          cam hex state', MTHexapod.EnabledSubstate(dstate.enabledSubstate), 
+      pd.to_datetime(dstate.private_sndStamp, unit='s'))
+
+    #m2hex
+    state = await m2hex.evt_summaryState.aget(timeout=5)
+    print('staring with: M2 hex state', salobj.State(state.summaryState), pd.to_datetime(state.private_sndStamp, unit='s'))
+    dstate = await m2hex.evt_controllerState.aget(timeout=5)
+    print('          hex state', MTHexapod.EnabledSubstate(dstate.enabledSubstate), 
+      pd.to_datetime(dstate.private_sndStamp, unit='s'))        
+        
+async def checkAOSSummaryStates(aos, m1m3, m2, camhex, m2hex):
+    #aos
+    sstate = await aos.evt_summaryState.aget(timeout=5)
+    print('starting with: MTAOS state',salobj.State(sstate.summaryState), pd.to_datetime(sstate.private_sndStamp, unit='s'))
+
+    await checkAOSCompStates(m1m3, m2, camhex, m2hex)
+
+async def checkSlewCompStates(ptg, mount, rot):
+    
+    sstate = await ptg.evt_summaryState.aget(timeout=5)
+    print('staring with: ptg state',salobj.State(sstate.summaryState), pd.to_datetime(sstate.private_sndStamp, unit='s'))
+    
+    sstate = await mount.evt_summaryState.aget(timeout=5)
+    print('staring with: mount state',salobj.State(sstate.summaryState), pd.to_datetime(sstate.private_sndStamp, unit='s'))
+
+    sstate = await rot.evt_summaryState.aget(timeout=5)
+    print('staring with: rot state',salobj.State(sstate.summaryState), pd.to_datetime(sstate.private_sndStamp, unit='s'))
 
 async def lowerM1M3(m1m3):
     m1m3.evt_detailedState.flush()
@@ -45,7 +101,33 @@ async def raiseM1M3(m1m3):
         if (MTM1M3.DetailedState(state.detailedState) == MTM1M3.DetailedState.ACTIVE
                 or MTM1M3.DetailedState(state.detailedState) == MTM1M3.DetailedState.ACTIVEENGINEERING):
             break
+
+async def readyM1M3(m1m3):
+    
+    m1m3Angle = await m1m3.tel_inclinometerData.next(flush=True, timeout=5)
+    print("m1m3 inclinometer Angle = ", m1m3Angle.inclinometerAngle)
+    
+    dstate = await m1m3.evt_detailedState.aget(timeout=200)
+    if MTM1M3.DetailedState(dstate.detailedState) == MTM1M3.DetailedState.PARKED:
+        await raiseM1M3(m1m3)
+    else:
+        print('M1M3 is raised')
         
+    m1m3HP = await m1m3.tel_hardpointActuatorData.aget()
+    print("m1m3 z position = ", m1m3HP.zPosition)
+    
+    m1m3ForceBalance = await m1m3.evt_appliedBalanceForces.aget(timeout=10.)
+    if not m1m3ForceBalance.forceMagnitude:
+        await m1m3.cmd_enableHardpointCorrections.set_start(timeout=10)
+        await asyncio.sleep(3.)
+    m1m3ForceBalance = await m1m3.evt_appliedBalanceForces.aget(timeout=10.)
+    print("Magnitude of the m1m3 force balance system", m1m3ForceBalance.forceMagnitude)
+    
+    ### no activeopticForces (or any other hunman-applied forces) to start with
+    fz = [0]*156
+    await m1m3.cmd_applyAberrationForces.set_start(zForces=fz)
+    await m1m3.cmd_applyActiveOpticForces.set_start(zForces=fz)
+    
 async def plotM1M3Forces(m1m3):
     
     fel = await m1m3.evt_appliedElevationForces.aget(timeout=10.)
@@ -130,23 +212,20 @@ async def plotM2Forces(m2):
     ax[1].set_title('applied forces')
     fig.colorbar(img, ax=ax[1])    
     
+async def readyM2(m2):
+    
+    zAngle = await m2.tel_zenithAngle.next(flush=True, timeout=5)
+    print('m2 inclinometer angle = ', zAngle.measured)
+    
+    m2ForceBalance = await m2.evt_forceBalanceSystemStatus.aget(timeout=10.)
+    if not m2ForceBalance.status:
+        await m2.cmd_switchForceBalanceSystem.set_start(status=True, timeout=10)
+    m2ForceBalance = await m2.evt_forceBalanceSystemStatus.aget(timeout=10.)
+    print("Status of the M2 force balance system", m2ForceBalance.status)
 
-async def readyHexForAOS(hex):
-    settings = hex.evt_settingsApplied.get()
-    print(settings.settingsVersion)
-    if not settings.settingsVersion[:12] == 'default.yaml':
-        print('YOU NEED TO SEND THIS HEXAPOD TO STANDBY, THEN LOAD THE PROPER CONFIG')
-    else:
-        lutMode = await hex.evt_compensationMode.aget(timeout=10)
-        if not lutMode.enabled:
-            await hex.cmd_setCompensationMode.set_start(enable=1, timeout=10)
-        print("compsensation mode enabled?",lutMode.enabled, pd.to_datetime(lutMode.private_sndStamp, unit='s'))
-        print("Does the hexapod has enough inputs to do LUT compensation?")
-        #Note: the target events are what the hex CSC checks; if one is missing, the entire LUT will not be applied
-        a = hex.evt_compensationOffset.get()
-        print(a.elevation, a.azimuth, a.rotation, a.temperature)
-        print('x,y,z,u,v,w = ', a.x, a.y, a.z, a.u, a.v, a.w)
-        
+    print('clear any M2 activeopticForces (or any other hunman-applied forces)')
+    await m2.cmd_resetForceOffsets.set_start()
+    
 async def moveHexTo0(hex):
     ### command it to collimated position (based on LUT)
     hex.evt_inPosition.flush()
@@ -177,4 +256,62 @@ async def printHexUncompensatedAndCompensated(hex):
     print(" ".join(f"{p:10.6f}" for p in [getattr(posC, i) for i in 'uvw']),'  ',
          pd.to_datetime(posC.private_sndStamp, unit='s'))
 
+async def readyHexForAOS(hex):
+    settings = hex.evt_settingsApplied.get()
+    hasSettings = 0
+    if hasattr(settings, 'settingsVersion'):
+        print(settings.settingsVersion)
+        hasSettings = 1
+    if (not hasSettings) or (not settings.settingsVersion[:12] == 'default.yaml'):
+        print('YOU NEED TO SEND THIS HEXAPOD TO STANDBY, THEN LOAD THE PROPER CONFIG')
+    else:
+        hexConfig = await hex.evt_configuration.aget(timeout=10.)
+        print("pivot at (%.0f, %.0f, %.0f) microns "%(hexConfig.pivotX, hexConfig.pivotY, hexConfig.pivotZ))
+        print("maxXY = ", hexConfig.maxXY, "microns, maxZ= ", hexConfig.maxZ, " microns")
+        print("maxUV = ", hexConfig.maxUV, "deg, maxW= ", hexConfig.maxW, " deg")
 
+        lutMode = await hex.evt_compensationMode.aget(timeout=10)
+        if not lutMode.enabled:
+            await hex.cmd_setCompensationMode.set_start(enable=1, timeout=10)
+        print("compsensation mode enabled?",lutMode.enabled, pd.to_datetime(lutMode.private_sndStamp, unit='s'))
+        print("Does the hexapod has enough inputs to do LUT compensation?")
+        #Note: the target events are what the hex CSC checks; if one is missing, the entire LUT will not be applied
+        a = hex.evt_compensationOffset.get()
+        print('mount elevation = ', a.elevation)
+        print('mount azimth = ', a.azimuth)
+        print('rotator angle = ', a.rotation)
+        print('? temperature = ', a.temperature)
+        print('x,y,z,u,v,w = ', a.x, a.y, a.z, a.u, a.v, a.w)
+        
+        posU = await hex.evt_uncompensatedPosition.aget(timeout=10.)
+        if abs(max([getattr(posU, i) for i in 'xyzuvw']))>0:
+            moveHexTo0(hex)
+        else:
+            print('hexapod already at LUT position')
+        
+async def moveMountConstantV(mount, startAngle, stopAngle):
+    #change the elevation angle step by step
+
+    freq = 1 #Hz
+    vAngle = 2 #1 deg change per minute
+    holdMinutes = 0.1 #how long to hold at integeter values of the elevation angle
+    angleStepSize = 1 #each time we change by 1 deg, before we hold in place
+
+    rampMinutes = angleStepSize/vAngle
+    print('This will run for %.0f minutes'%((startAngle - stopAngle)*(rampMinutes+holdMinutes)))
+    start_time = Time(datetime.now())
+    startTime = time.time()
+    end_time = start_time + timedelta(minutes=80)
+    demandAngle = startAngle
+    while demandAngle > stopAngle-0.01:
+        await asyncio.sleep(1.0/freq)
+
+        timeNow = time.time()
+        minutesEllapsed = (timeNow - startTime)/60
+        cyclePassed = np.floor(minutesEllapsed/(rampMinutes+holdMinutes))
+        minutesIntoThisCycle = min(rampMinutes, minutesEllapsed - cyclePassed*(rampMinutes+holdMinutes))
+        demandAngle = startAngle -  cyclePassed*angleStepSize - minutesIntoThisCycle * vAngle
+        #print(demandAngle, cyclePassed, minutesIntoThisCycle)
+        await mount.cmd_moveToTarget.set_start(azimuth=0, elevation=demandAngle)
+        
+        
